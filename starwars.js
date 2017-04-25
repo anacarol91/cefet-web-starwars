1e22099b91309bb06035cbaba331cd7d7def86f3
Ana Carolina Guimarães <anacgp@gmail.com>
2017-04-25 12:17:54 -0300

    incompleto

--- starwars.js
+++ starwars.js
@@ -3,3 +3,25 @@
 // para carregar:
 //  - A lista de filmes
 //  - A introdução de cada filme, quando ele for clicado
+
+$.ajax({
+  url: 'https://swapi.co/api/films/',
+  method: 'GET',      // opcional: 'GET' é o valor padrão
+  success: function(resposta) {
+
+    let filmes = resposta.results;
+    window.localStorage.setItem('filmes', filmes);
+    console.dir(filmes); 
+
+    for(filme of filmes){
+    	console.dir(filme.title); 
+    	$( "ul" ).append("<li data-episode-url=\"" + filme.url + "\">" + filme.title + "</li>");
+
+    	$("li").click(function() {
+    		console.dir(filme.opening_crawl); 
+    		$("pre").text(filme.opening_crawl);
+    	});
+    }
+  }  
+});
+
